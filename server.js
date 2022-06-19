8b83ea34589c66af4e670204c5c4517841a79af7
Jun Ahn <junahn45@gmail.com>
2022-06-19 14:50:32 -0700

    set up

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+/* eslint linebreak-style: ["error", "windows"] */
 const express = require('express');
 
 const app = express();
 const port = 8080;
+const db = {};
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello Worldddd!');
+});
+
+app.post('/addUser', (req, res) => {
+  console.log(req.body);
+  // send HTTP 200 as a response to tell the user it has been
+  res.sendStatus(200);
 });
 
 app.listen(port, () => {
